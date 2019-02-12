199d6373112f8c9f45b779330e68bfe88bc2d3e5
xzdandy <xzdandy@gmail.com>
2019-02-12 21:08:49 +0000

    refactory the code a bit

--- opencv_darkflow.py
+++ opencv_darkflow.py
@@ -1,9 +1,9 @@
 import cv2
 import os, sys
 
-from darkflow.defaults import argHandler
 from darkflow.net.build import TFNet
-from darkflow.net import flow
+
+DARKFLOW_HOME="/home/device/darkflow"
 
 """
 Darkflow implementation of image recognition ported into a class
@@ -11,39 +11,35 @@ using OpenCV
 """
 class opencv_darkflow:
 
-    def __init__(self, dir = '.', confidence = 0.5, gpu = 0.0):
-        self.GPU = gpu
-        self.IMG_DIR = dir
-        self.FG = argHandler()
+    def __init__(self, model = 'yolo.cfg', load = 'yolo.weights', confidence = 0.1):
         
-        self.construct_tfnet(confidence)
-
-    def construct_tfnet(self, confidence):
-        self.FG.setDefaults()
-
-        args = ['flow', '--imgdir', self.IMG_DIR, '--threshold', str(confidence), '--model', 'cfg/yolo.cfg'
-        , '--load', 'bin/yolo.weights', '--gpu', str(self.GPU)]
-
-        self.FG.parseArgs(args)
+        basecfg = os.path.join(DARKFLOW_HOME, 'cfg')
+        basebin = os.path.join(DARKFLOW_HOME, 'bin')
         
-        try: self.FG.load = int(self.FG.load) 
-        except: pass
-
-        sys.stdout = open(os.devnull, 'w') # Block stdout print
-        self.tfnet = TFNet(self.FG)
-        sys.stdout = sys.__stdout__ # Enable stdout print
+        options = {'config' : basecfg,
+                   'model' : os.path.join(basecfg, model),
+                   'load' : os.path.join(basebin, load),
+                   'threshold' : confidence}
+        
+        #sys.stdout = open(os.devnull, 'w') # Block stdout print
+        self.tfnet = TFNet(options)
+        #sys.stdout = sys.__stdout__ # Enable stdout print
 
     # Reformat bounding box array to only return coordinates
-    def bb_reformat(self, arr):
+    def bb_reformat(self, arr, labels):
+
         ret_arr = []
 
         for bb in arr:
-            (x1, y1) = (bb['topleft']['x'], bb['topleft']['y'])
-            (x2, y2) = (bb['bottomright']['x'], bb['bottomright']['y'])
             
-            ret_arr.append((x1, y1, x2, y2))
+            if bb['label'] in labels:
+                
+                (x1, y1) = (bb['topleft']['x'], bb['topleft']['y'])
+                (x2, y2) = (bb['bottomright']['x'], bb['bottomright']['y'])
+            
+                ret_arr.append((x1, y1, x2, y2))
 			
         return ret_arr
 
-    def detect(self, frame):
-        return self.bb_reformat(self.tfnet.return_predict(frame))
+    def detect(self, frame, labels = {'bus', 'car', 'truck'}):
+        return self.bb_reformat(self.tfnet.return_predict(frame), labels)
